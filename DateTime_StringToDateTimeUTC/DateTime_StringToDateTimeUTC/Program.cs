f8a5bcddc516ee5fd3beb6a662ec6cf2431fefa1
vdhavle <visharad.dhavle@travelex.com>
2013-09-30 13:11:24 +0100

    New Project - DateTime_StringToDateTimeUTC : Added

---
+++ DateTime_StringToDateTimeUTC/DateTime_StringToDateTimeUTC/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace DateTime_StringToDateTimeUTC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dt = "30/9/2013 10:43 AM";
+            DateTime dtime = new DateTime();
+            dtime = DateTime.Parse(dt).ToUniversalTime();
+
+
+        }
+    }
+}
